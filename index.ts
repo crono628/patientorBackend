1bfb5c26b4dc4b26e83eec085a32b47a8e680030
crono628 <84817352+crono628@users.noreply.github.com>
2023-04-12 20:41:18 -0400

    re-initializing project. starting patientor

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
 import express from 'express'
-import diaryRouter from './routes/diaries'
-
 const app = express()
 app.use(express.json())
 
@@ -10,8 +8,6 @@ app.get('/ping', (_req, res) => {
   console.log('someone pinged here')
   res.send('pong')
 })
-
-app.use('/api/diaries', diaryRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
